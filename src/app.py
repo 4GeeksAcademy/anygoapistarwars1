c44fdc0924da36c7bbaef9ebc38410bfe8166d3d
darosagomezany <139292566+darosagomezany@users.noreply.github.com>
2023-10-02 00:55:13 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet, Fav_Planets, Fav_People
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,115 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route("/user", methods=["GET"])
+def get_all_users():
+    all_users = User.query.all()
+    all_users = list(map(lambda user: user.serialize(), all_users))
+    return jsonify(all_users), 200
+    return jsonify({
+        "mensaje": "aca deben estar todos los usuarios"
+    })
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+
+@app.route("/people", methods=["GET"])
+def get_all_people():
+    all_people = People.query.all()
+    all_people = list(map(lambda people: people.serialize(), all_people))
+    return jsonify(all_people), 200
+    return jsonify({
+        "mensaje": "aca deben estar todos los personajes de sw"
+    })
+
+
+@app.route("/planet", methods=["GET"])
+def get_all_planet():
+    all_planet = Planet.query.all()
+    all_Planet = list(map(lambda planet: planet.serialize(), all_planet))
+    return jsonify(all_planet), 200 
+    return jsonify({
+        "mensaje": "aca deben estar todos los planetas de sw"
+    })        
+    
+
+
+@app.route("/people/<int:id>", methods=["GET"])
+def get_one_people(people_id):
+    onepeople = People.query.get(people_id)
+    return jsonify(onepeople.serialize())
+    
+
+@app.route("/planet/<int:id>", methods=["GET"])
+def get_one_planet(planet_id):
+    oneplanet = Planet.query.get(planet_id)
+    return jsonify(oneplanet.serialize())
+    
+
+@app.route("/planet/favorite/<int:planet_id>", methods=["POST"])
+def post_fav_planet(planet_id):
+    one = Planet.query.get(planet_id)
+    user = User.query.get(1)
+    if(one):
+        new_fav = Fav_planets()
+        new_fav.email = user.email
+        new_fav.planet_id = planet_id
+        db.session.add (new_fav)
+        db.session.commit()
+        return jsonify({
+        "mensaje": "planeta favorito agregado"
+    })
+    else:
+        raise APIException("No existe planeta", status_code=404)
+
+
+@app.route("/people/favorite/<int:people_id>", methods=["POST"])
+def post_fav_people(people_id):
+    one = People.query.get(people_id)
+    user = User.query.get(1)
+    if(one):
+        new_fav = Fav_People()
+        new_fav.email = user.email
+        new_fav.people_id = people_id
+        db.session.add (new_fav)
+        db.session.commit()
+        return jsonify({
+        "mensaje": "el personaje con id" + str(people_id) + "ha sido agregado"
+    })
+    else:
+        raise APIException("No existe personaje", status_code=404)
+
+
+@app.route("/planet/favorite/<int:planet_id>", methods=["DELETE"])
+def delete_fav_planet(planet_id):
+    one = Fav_Planet.query.filter_by(planet_id=planet_id).first()
+    user = User.query.get(1)
+    if(one):
+        db.session.delete(one)
+        db.session.commit()
+        return jsonify({
+        "mensaje": "el planeta con id" + str(planet_id) + "ha sido eliminado"
+    })
+    else:
+        raise APIException("No existe planeta", status_code=404)
+
+
+@app.route("/favorite/people/<int:people_id>", methods=["DELETE"])
+def delete_fav_people(people_id):
+    one = Fav_People.query.filter_by(people_id=people_id).first()
+    user = User.query.get(1)
+    if(one):
+        db.session.delete(one)
+        db.session.commit()
+        return jsonify({
+        "mensaje": "el personaje con id" + str(people_id) + "ha sido eliminado"
+    })
+    else:
+        raise APIException("No existe personaje seleccionado", status_code=404)
+
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -17,3 +17,67 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+         #"hola, me llamo ${nombre}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.name,
+         }
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.name,
+         }
+
+
+class Fav_People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    people_id = db.Column(db.String(120), db.ForeignKey("people.id"))
+    email = db.Column(db.String(120), db.ForeignKey("user.email"))
+    rel_people = db.relationship("People")
+    rel_user = db.relationship("User")
+
+    def __repr__(self):
+        return '<Fav_people %r>' % self.email
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "people_name": self.people_name,
+         }
+
+class Fav_Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_id = db.Column(db.String(120), db.ForeignKey("planet.id"))
+    email = db.Column(db.String(120), db.ForeignKey("user.email"))
+    rel_planet = db.relationship("Planet")
+    rel_user = db.relationship("User")
+
+    def __repr__(self):
+        return '<Fav_planet %r>' % self.email
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "planet_name": self.planet_name,
+         }
